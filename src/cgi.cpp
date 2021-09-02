f0b5634046f54c706eb3db3fd14088c5fed0e6bd
zainabdnaya <diyanazizo13@gmail.com>
2021-09-02 18:27:35 +0100

    Working on CGI1.1

--- src/cgi.cpp
+++ src/cgi.cpp
@@ -11,34 +11,51 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#define die(e) do { fprintf(stderr, "%s\n", e); exit(EXIT_FAILURE); } while (0);
+
 int main(int argc, char *argv[], char *envp[])
 {
-    int fd;
-    char buf[1024];
-    int n;
-    int i;
-    int pid;
-    int status;
-    int fd_in;
-    int fd_out;
-    int fd_err;
-    int fd_pipe[2];
-
-    if (argc != 2) {
-        printf("Usage: %s <filename>\n", argv[0]);
-        exit(1);
-    }
-    /*
-
-    export GATEWAY_INTERFACE="CGI/1.1"
-	export SERVER_PROTOCOL="HTTP/1.1"
-	export SERVER_PORT="8011"
-	export REQUEST_METHOD="GET"
-	export SERVER_NAME="localhost";
-	export REDIRECT_STATUS=1;
-	export REMOTE_ADDR="127.0.0.1";
-	export PATH_INFO="php-cgi7.4";
+  int link[2];
+  pid_t pid;
+  char foo[4096];
 
 
-    */
+    setenv("GATEWAY_INTERFACE","CGI/1.1",1);
+	setenv("SERVER_PROTOCOL","HTTP/1.1",1);
+	setenv("REDIRECT_STATUS","1",1);
+	setenv("PATH_INFO","/Users/zdnaya/Desktop/WebServe/webpage/cgi/home.php",1);
+	setenv("SERVER_PORT","8011",1);
+	setenv("REQUEST_METHOD","POST",1);
+	setenv("REMOTE_ADDR","127.0.0.1",1);
+    setenv("GATEWAY_INTERFACE","CGI/1.1",1);
+	setenv("SERVER_NAME","localhost",1);
+    setenv("SERVER_PROTOCOL","HTTP/1.1",1);
+    setenv("SCRIPT_FILENAME","/Users/zdnaya/Desktop/WebServe/webpage/cgi/home.php",1);
+    setenv("SCRIPT_NAME","home.php",1);
+    setenv("REDIRECT_STATUS","200",1);
+
+  if (pipe(link)==-1)
+    die("pipe");
+
+  if ((pid = fork()) == -1)
+    die("fork");
+
+  if(pid == 0) {
+
+    dup2 (link[1], STDOUT_FILENO);
+    close(link[0]);
+    close(link[1]);
+    execve("/Users/zdnaya/goinfre/.brew/bin/php-cgi", argv, envp);
+    die("execl");
+
+  } else {
+
+    close(link[1]);
+    int nbytes = read(link[0], foo, sizeof(foo));
+    printf("Output: (%.*s)\n", nbytes, foo);
+    wait(NULL);
+
+  }
+  return 0;
+
 }
--- webpage/cgi/home.php
+++ webpage/cgi/home.php
@@ -1,12 +1,22 @@
-<?php
-  
-?>
-<html>
-   <body>
-      
-      <form action="" method="POST" enctype="multipart/form-data">
-         <input type="file" name="image" />
-         <input type="submit"/>
-      </form>
-   </body>
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        <?php
+            echo HELLO;
+        ?>
+    </title>
+</head>
+<body>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="uploaded_file" id="uploaded_file">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+</body>
 </html>
+
